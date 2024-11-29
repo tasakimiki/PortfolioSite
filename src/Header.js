08f58ec2ffa7e504e1861ba7e9afa43cc50db4a8
Tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-11-29 22:58:24 +0900

    ヘッダーメニューの色変更修正

--- src/Header.js
+++ src/Header.js
@@ -12,13 +12,13 @@ function Header(){
                 </a>
                 <nav>
                     <ul className="header_menu flex anton">
-                        <li className = {`header_menu1${location.pathname === "/portfolio/all"? "_active":""}`}>
+                        <li className = {`header_menu1${location.pathname.startsWith("/portfolio")? "_active":""}`}>
                             <Link to={"/portfolio/all"}><span>P</span>ORTFOLIO</Link>
                         </li>
-                        <li className = {`header_menu2${location.pathname === "/profile"? "_active":""}`}>
+                        <li className = {`header_menu2${location.pathname.startsWith("/profile")? "_active":""}`}>
                             <Link to={"/profile"}><span>P</span>ROFILE</Link>
                         </li>
-                        <li className = {`header_menu3${location.pathname === "/contact"? "_active":""}`}>
+                        <li className = {`header_menu3${location.pathname.startsWith("/contact")? "_active":""}`}>
                             <Link to={"/contact"}><span>C</span>ONTACT</Link>
                         </li>
                     </ul>
