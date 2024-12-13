0758db11e55a6842f1b2bb400c9da587ba31c835
Tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-12-13 13:58:23 +0900

    カルーセルを作り隊

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,9 +5,18 @@ import images from "./DisplayImages/Images/TextData/ImagesTextData.json";
 
 function HomePage(){
     return(
-        <div className = "main">
+        <div className = "home_page">
             <div className = "image_slider">
-                <h1>これはホーム画面！！！！</h1>
+                <div className = "slider_item_container">
+                    <div className = "slider_item">1</div>
+                    <div className = "slider_item">2</div>
+                    <div className = "slider_item">3</div>
+                    <div className = "slider_item">4</div>
+                    <div className = "slider_item">5</div>
+                </div>
+            </div>
+            <div className = "">
+
             </div>
         </div>
     );
