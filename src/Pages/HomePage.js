93ecbb9e1113d5f3b6261cb36724bc5db7e02b7a
Tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-12-18 02:05:12 +0900

    カルーセル作り隊

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -7,25 +7,34 @@ function HomePage(){
     const item_count = 5;
     const copy_count = 2;
 
-    const [current_item, set_current_item] = useState(0);
-    const prev_item = () => set_current_item((n) => n + 1)
-    const next_item = () => set_current_item((n) => n - 1)
+    const item_numbers = [...Array(item_count)].map((_, i) => i + 1);
+    //...を使うことで空の配列が展開されてmapメソッドが使えるようになるらしい
+    //map
+
+    const [current_item, set_current_item] = useState(0); //いまいるところ、値は0~4
+
+    //戻るボタン
+    const prev_item = () => {
+        set_current_item((n) => (n - 1 + item_count) % item_count); //説明できないけどこれすごい
+    };
+
+    //次へボタン
+    const next_item = () => {
+        set_current_item((n) => (n + 1) % item_count);
+    };
 
     return(
         <div className = "home_page">
-
             <div className = "image_slider">
                 <div className = {`slider_item_container item_${current_item}`}>
-                    <div className = "slider_item">1</div>
-                    <div className = "slider_item">2</div>
-                    <div className = "slider_item">3</div>
-                    <div className = "slider_item">4</div>
-                    <div className = "slider_item">5</div>
+                    {item_numbers.map((n, index) => (
+                        <div key = {index} className = "slider_item">{n}</div>
+                    ))}
                 </div>
             </div>
             <div className = "slider_buttons">
-                <button onClick = {prev_item}></button>
-                <button onClick = {next_item}></button>
+                <button className = "slide_button" onClick = {prev_item}>&lt;</button>
+                <button className = "slide_button" onClick = {next_item}>&gt;</button> {/*&gt;とかは<と>を表示するためのHTMLエンティティとやららしいです*/}
             </div>
         </div>
     );
