fee68d14ee0331e2576a744497c15d22db162c5b
Tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-12-21 20:21:57 +0900

    新着画像5枚をスライドに表示

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,42 +1,110 @@
-import React, {useState} from 'react';
-import {Link} from "react-router-dom";
-import {Route, Routes }from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination'; // ページネーション用スタイル
+import 'swiper/css/navigation'; // ナビゲーション用スタイル
+import { Pagination, Navigation } from 'swiper/modules'; // Swiper モジュールのインポート
 import images from "./DisplayImages/Images/TextData/ImagesTextData.json";
 
 function HomePage(){
-    const item_count = 5;
+    let [slider_images, set_slider_images] = useState([...Array(5)].map(() => 0));
 
-    const item_numbers = [...Array(item_count)].map((_, i) => i + 1);
-    //...を使うことで空の配列が展開されてmapメソッドが使えるようになるらしい
-    //map
+    const add_images = () => {
+        const new_slider_images = [...slider_images];
 
-    const [current_item, set_current_item] = useState(0); //いまいるところ、値は0~4
+        images.forEach((image, index) =>{ 
 
-    //戻るボタン
-    const prev_item = () => {
-        set_current_item((n) => (n + 1) % item_count); //説明できないけどこれすごい
+            if(index !== 0){
+                let larger_flag = 0;
+                let tmp = 0;
+                let i;
+                
+                for(i = 4; i >= 0; i--){
+                    console.log(images[slider_images[i]].project_num, images[index].project_num, index);
+                    console.log(images[slider_images[i]].year, image.year, image.project_num);
+
+                    if (images[new_slider_images[i]].year < image.year){
+                        larger_flag = 1; //制作年を比べ、大きかったらフラグを立てる
+                        tmp = index;
+
+                        console.log(larger_flag);
+
+                    } else if(images[new_slider_images[i]].year === image.year){
+
+                        console.log(images[slider_images[i]].month, image.month, image.project_num);
+
+                        if(images[new_slider_images[i]].month < image.month){
+                            larger_flag = 1;
+                            tmp = index;
+
+                            console.log(larger_flag);
+                        }
+                        else if(images[new_slider_images[i]].month === image.month){
+                            larger_flag = 1;
+                            tmp = index;
+
+                            console.log(larger_flag);
+                            break;
+                        }
+                        else{
+
+                            console.log(larger_flag);
+                            break;
+                        }
+    
+                    }
+                    else{
+                        console.log(larger_flag);
+
+                        break;
+                    }
+                }
+
+                if(larger_flag === 1){
+                    i = i + 1;
+
+                    for (let j = 4; j > i; j--){
+                        new_slider_images[j] = new_slider_images[j - 1];
+                    }
+                    
+                    new_slider_images[i] = tmp;
+                    
+                    console.log(larger_flag, i, tmp)
+                    console.log(new_slider_images)
+
+                }
+                else{
+                    console.log(larger_flag)
+                }
+            }
+        });
+        set_slider_images(new_slider_images);
     };
 
-    //次へボタン
-    const next_item = () => {
-        set_current_item((n) => (n - 1) + item_count % item_count);
-        console.log("なくした");
-        console.log("ばぐしゅうせい");
-    };
+    useEffect(() => {
+        add_images();
+    }, []); //初期レンダリング時に1度だけ実行
 
     return(
-        <div className = "home_page">
-            <div className = "image_slider">
-                <div className = {`slider_item_container item_${current_item + 1}`}>
-                    {item_numbers.map((n, index) => (
-                        <div key = {index} className = "slider_item">{n}</div>
-                    ))}
-                </div>
-            </div>
-            <div className = "slider_buttons">
-                <button className = "slide_button" onClick = {prev_item}>&lt;</button>
-                <button className = "slide_button" onClick = {next_item}>&gt;</button> {/*&gt;とかは<と>を表示するためのHTMLエンティティとやららしいです*/}
-            </div>
+        <div className = "image_slider">
+            <Swiper
+             slidesPerView = {3} //一度に表示するスライドの数
+             pagination={{
+                clickable: true, //何枚目のスライドか示す丸いやつ
+             }}
+             navigation //左右に矢印のナビゲーションを追加
+             loop = {true}
+             modules = {[Pagination, Navigation]}
+             >
+
+                {slider_images.map((slider_image, index) => {
+                    return(
+                        <SwiperSlide key = {index}>
+                            <img className = "slider_item" src = {images[slider_image].path} alt = {`image${index}`} />
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
         </div>
     );
 }
