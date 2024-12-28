1c85214a84d6371fdd6816417437a5d157db1225
Tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-12-28 16:55:03 +0900

    新規イラスト2枚を追加

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -89,7 +89,6 @@ function HomePage(){
         <div className = "image_slider">
             <Swiper
              slidesPerView = {3} //一度に表示するスライドの数
-             spaceBetween = {200} //スライド間のスペース
              pagination={{
                 clickable: true, //何枚目のスライドか示す丸いやつ
              }}
